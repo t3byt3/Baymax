13261a0c495fbbf34690657e2101197b670f7b36
Anirudh Murali <m.anirudh18@gmail.com>
2016-06-05 12:23:47 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
 $(function () {
-  
-  var die = ["R.I.P", "May mah soul rest in peace", "Good joke lame a*s","Is it no problem , if I shutdown ??"];
+
+
+  var blabber_check=0;
+
+  var db = [ ["what","your","name"],
+             ["hello"],
+             ["hey"],
+             ["there"],
+             ["hi"]
+
+
+           ];
+
+  var answers = [ ["Baymax","Personal Assistant , .. , on health care "],
+                  ["yes","hi","hello","ask me ","I am always here"],
+                  ["yes","hi","hello","ask me ","I am always here"],
+                  ["yes","hi","hello","ask me ","I am always here"],
+                  ["yes","hi","hello","ask me ","I am always here"]
+
+               ];
   var close=3;
-  
-  var machineName = "Lol";
+
+  var machineName = "Baymax";
   $(".status-text").hide();
   function addMessage(message, author, machine) {
-    $("#messages").append('<li style="display: none;"><strong title="' + author + '">' + author + ': </strong><span>' + message +  '</span></li>');
+    $("#messages").append('<li class="listStyle" style="display: none;"><span style="color:lightblue">' + author + ': </span><span style="color:green">' + message +  '</span></li>');
     if (typeof machine == "boolean")
       $(".status-text").fadeOut();
     else
@@ -14,44 +32,93 @@ $(function () {
     $("#messages li:last").fadeIn();
     if (typeof machine != "boolean")
       setTimeout(function () {
-        
-        switch (message.toLowerCase()) {
+
+        var i=0;
+        var j=0;
+        var randomNumber;
+        for(i=0;i<db.length;i++)
+        {
+            j=0;
+            for(j=0;j<db[i].length;j++)
+            {
+                if(!message.includes(db[i][j]))
+                {
+                    //addMessage("not found",machineName,true);
+                    break;
+                }
+                if(j==db[i].length-1)
+                {
+                    randomNumber = Math.floor(Math.random()*answers[i].length);
+                    addMessage(answers[i][randomNumber],machineName,true);
+                }
+            }
+        }
+        if(message.includes('book'))
+        {
+            addMessage("Go on for a appointment ", machineName, true);
+            $(".but_1").show();
+        }
+
+
+        /*switch (message.toLowerCase()) {
+
           case "book":
-            
-            addMessage("Hqllo, " + localStorage.username + "! How are you?", machineName, true);
-          
+
+            addMessage("Hello, " + localStorage.username + "! How are you?", machineName, true);
+
         $(".but_1").show();
-            
             break;
+
+
           case "hello":
-          case "ff":
-            addMessage("aa, ", machineName, true);
+          case "hi":
+            addMessage("Hey there! Have a good day.", machineName, true);
             break;
+
+
+
           case "die":
-            var randomNumber = Math.floor(Math.random()*die.length);
-            addMessage(die[randomNumber], machineName, true);
+            var randomNumber = Math.floor(Math.random()*sample.length);
+            addMessage(sample[randomNumber], machineName, true);
             close--;
             if (close==0)
               {
+                alert("Thanks for stopping by !! :)");
                 window.close();
               }
-            
+
             break;
+
+
+
+
           case "help":
           case "manual":
             addMessage("You can use the following commands:", machineName, true);
             break;
+
+
+
           case "time":
             addMessage("The time now is: see the bottom right clock is there, ha ha.", machineName, true);
             break;
+
+
+
           case "what is your name?":
           case "what is your name":
             addMessage("My name is " + machineName + ". I thought you knew it already!", machineName, true);
             break;
+
+
+
           default:
             addMessage("What are you blabbering?", machineName, true);
             break;
-        }
+
+
+
+        } */
       }, 500);
   }
   if (typeof localStorage != "undefined")
